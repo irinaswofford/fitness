9330658789a35f9641d48719857dd71fe1fa9d9c
irinaswofford <irinaswofford@gmail.com>
2020-05-25 22:04:08 -0500

    Updates

---
+++ precache-manifest.b836c428c0cc01948551a26dd9945f9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48fb15a21d06c7e985fe64c26648cd4",
+    "url": "/fitness/index.html"
+  },
+  {
+    "revision": "cb7c7729e08c830ac6f2",
+    "url": "/fitness/static/css/2.6127c4b5.chunk.css"
+  },
+  {
+    "revision": "b756ad61471e887f2798",
+    "url": "/fitness/static/css/main.e6d340d9.chunk.css"
+  },
+  {
+    "revision": "cb7c7729e08c830ac6f2",
+    "url": "/fitness/static/js/2.1e730e7e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fitness/static/js/2.1e730e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b756ad61471e887f2798",
+    "url": "/fitness/static/js/main.62536d98.chunk.js"
+  },
+  {
+    "revision": "05373ac35cfc6dc797ba",
+    "url": "/fitness/static/js/runtime-main.ef800f66.js"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/fitness/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/fitness/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/fitness/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "1a52abf409c29b0e56dad653253c9f89",
+    "url": "/fitness/static/media/hero_shadow.1a52abf4.jpg"
+  }
+]);
